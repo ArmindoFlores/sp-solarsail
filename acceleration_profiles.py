226098a3cdc775e96ef25c70f397d49d298c1f37
ArmindoFlores <francisco.rodrigues0908@gmail.com>
2023-07-19 00:58:20 +0100

    Fixed an issue with acceleration profiles

--- acceleration_profiles.py
+++ acceleration_profiles.py
@@ -27,6 +27,22 @@ def escape_up(t, y, orbital_params, reflectivness, area, mass):
         return solar_radiation_accel
     return e_r * 0
 
+def escape_earth(t, y, orbital_params, reflectivness, area, mass):
+    point = np.array([y[0], y[1]])
+    center = point - np.array([-AU*1e-3, 0])
+    vel = np.array([y[2], y[3]])
+    e_r = point - orbital_params._focus
+    r = np.linalg.norm(e_r)
+    e_vec = np.cross(vel, np.cross(np.array([*e_r, 0]), vel))[:2] / orbital_params._mu - e_r / r
+    if np.cross(e_vec, e_r) > 0:
+        sun_e_r = point - center
+        sun_r = np.linalg.norm(sun_e_r)
+        sun_e_r /= sun_r
+        e_r /= r
+        solar_radiation_accel = 1e-3 * radiation_pressure(sun_r * 1e3, reflectivness) * sun_e_r * area / mass
+        return solar_radiation_accel
+    return e_r * 0
+
 def always(t, y, orbital_params, reflectivness, area, mass):
     point = np.array([y[0], y[1]])
     e_r = point - orbital_params._focus
@@ -35,9 +51,9 @@ def always(t, y, orbital_params, reflectivness, area, mass):
     solar_radiation_accel = 1e-3 * radiation_pressure(r * 1e3, reflectivness) * e_r * area / mass
     return solar_radiation_accel
     
-def tangential(t, y, orbital_params, mag=0.000007):
+def tangential(t, y, orbital_params, mag):
     point = np.array([y[0], y[1]])
-    e_r = point - orbital_params.focus
+    e_r = point - orbital_params._focus
     e_t = np.array([-e_r[1], e_r[0]])
     e_t /= np.linalg.norm(e_t)
     return e_t * mag
